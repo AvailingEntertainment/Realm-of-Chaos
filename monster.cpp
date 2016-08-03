a72c083749b623466f63ba719afb0e7b0b23aea2
Logan Cunningham <availingentertainment@gmail.com>
2016-08-03 13:52:21 -0700

    12. Mob Platform

---
+++ monster.cpp
@@ -0,0 +1,17 @@
+#include "monster.h"
+
+monster::mobType() {
+    
+}
+
+monster::mobName() {
+    
+}
+
+monster::attackType() {
+    
+}
+
+monster::mobStats(int id) {
+    
+}
---
+++ monster.h
@@ -0,0 +1,14 @@
+#ifndef MONSTER_H
+#define MONSTER_H
+
+class monster {
+public:
+    void mobType();
+    void mobName();
+    void attackType();
+    void mobStats(int);
+private:
+
+};
+
+#endif 
